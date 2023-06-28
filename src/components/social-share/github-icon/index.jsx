a6a4bae6e6b52692baf087bc0a96af968b0e122e
minwoo9629 <minwoo9629@gmail.com>
2023-06-28 21:54:26 +0900

    feat[icon] : Edit github link

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/minwoo9629"
       className="github"
       aria-label="GitHub"
       target="_blank"
